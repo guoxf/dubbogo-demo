e82a59e1fa9ff986aef6bc4f84b58328aa00697a
elvin <ants.guoxf@gmail.com>
2021-11-18 11:20:21 +0800

    bug测试

---
+++ Makefile
@@ -0,0 +1,2 @@
+run:
+	DUBBO_GO_CONFIG_PATH="./dubbogo.yaml" go run main.go
---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"context"
+
+	"dubbo.apache.org/dubbo-go/v3/config"
+	_ "dubbo.apache.org/dubbo-go/v3/imports"
+	apipb "github.com/guoxf/dubbogo-demo/api"
+)
+
+type HelloWorldProvider struct {
+	apipb.UnsafeHelloWorldServer
+}
+
+func (h *HelloWorldProvider) SayHello(ctx context.Context, in *apipb.SayHelloRequest) (*apipb.SayHelloResponse, error) {
+	return &apipb.SayHelloResponse{}, nil
+}
+
+// gin-swagger middleware
+// swagger embed files
+func main() {
+	config.SetProviderService(&HelloWorldProvider{})
+	if err := config.Load(); err != nil {
+		panic(err)
+	}
+	select {}
+}
